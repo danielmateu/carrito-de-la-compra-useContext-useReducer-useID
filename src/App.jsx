71f77b0633c181aedfcb9ea3f508dbde3229b9ab
Daniel Mateu <danielmateu86@gmail.com>
2023-03-02 16:19:25 +0100

    Creacion customHook useFilters

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,14 @@ import { Header } from "./components/Header";
 import { Products } from "./components/Products"
 import { products as initialProducts } from './mocks/products.json';
 
+const useFilters = () => {
 
-function App() {
-
-  const [products, setProducts] = useState(initialProducts)
-  //Estado para los Filtros
   const [filters, setFilters] = useState({
     search: '',
-    // price: 'all',
     category: 'all',
     minPrice: 0,
   })
 
-  // Metodo para filtrar productos por categoria
   const filterProducts = (products) => {
     return products.filter((product) => {
       return (
@@ -29,6 +24,33 @@ function App() {
     })
   }
 
+  return {filterProducts, setFilters} 
+}
+
+function App() {
+
+  const [products, setProducts] = useState(initialProducts)
+  //Estado para los Filtros
+  // const [filters, setFilters] = useState({
+  //   search: '',
+  //   // price: 'all',
+  //   category: 'all',
+  //   minPrice: 0,
+  // })
+
+  // Metodo para filtrar productos por categoria
+  // const filterProducts = (products) => {
+  //   return products.filter((product) => {
+  //     return (
+  //       product.price >= filters.minPrice &&
+  //       (
+  //         filters.category === 'all' ||
+  //         product.category === filters.category
+  //       )
+  //     )
+  //   })
+  // }
+  const {filterProducts, setFilters} = useFilters()
   const filteredProducts = filterProducts(products)
 
   return (
