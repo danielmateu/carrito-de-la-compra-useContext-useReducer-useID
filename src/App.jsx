e5fa5d3880491913549637620e884771405f9599
Daniel Mateu <danielmateu86@gmail.com>
2023-03-02 15:23:29 +0100

    Filtro por precio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import {  CartIcon } from "./components/Icons"
 import { Products } from "./components/Products"
-import { products } from './mocks/products.json';
+import { products as initialProducts} from './mocks/products.json';
 
 
 function App() {
 
+  const [products, setProducts] = useState(initialProducts)
+  //Estado para los Filtros
+  const [filters, setFilters] = useState({
+    search: '',
+    // price: 'all',
+    category: 'all',
+    minPrice: 0,
+  })
 
+  // Metodo para filtrar productos por categoria
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice && 
+          (
+            filters.category === 'all' ||
+            product.category === filters.category
+          )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+  
   return (
     <>
       <h1>Shopping Card <CartIcon/></h1>
-
-      <Products
-        products={products}
+      <Products products={filteredProducts}
+        
       />
     </>
   )
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,19 +1,18 @@
 import './Products.css';
 import { AddToCartIcon } from './Icons';
-// Import data from products.json
-
 
 export const Products = ({products}) => {
+
     return (
         <main className='products'>
             <ul>
-                {products.map((product) => (
+                {products.slice(0,12).map((product) => (
                     <li key={product.id}>
                         <img src={product.thumbnail} alt={product.title} />
                         <div>
                             <h3>{product.title}</h3>
                             <p>{product.description}</p>
-                            <p>{product.price}</p>
+                            <strong>{product.price}</strong> €
                         </div>
                         <button className='addToCart'>
                             <AddToCartIcon />
